78673b16bd92442d3536a1cfc2fec9382c8f815f
Leo <leonbauti02@gmail.com>
2024-10-16 17:05:06 -0300

    agregue el controlador y rutas para la tabla 'platforms' con sus respectivas fotos de prueba en postman

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ const app = express();
 app.use(express.json());
 
 // RUTAS
+// tablas de la base de datos
 app.use('/db-Table-raw/games', require('./routes/games.routes'));
-app.use('/db-Table-raw/tags', require('./routes/tags.routes'))
-
+app.use('/db-Table-raw/tags', require('./routes/tags.routes'));
+app.use('/db-Table-raw/platforms', require('./routes/platforms.routes'));
+// lista con información completa de los juegos
 app.use('/games', require('./routes/gamesList.routes'));
 
 
